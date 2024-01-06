6fce9d6a5aeb4af494c2a2adcbfca0c9fa46d9f1
AaronGao2028 <a43gao@uwaterloo.ca>
2024-01-05 19:50:37 -0500

    Updated experience section

---
+++ script.js
@@ -0,0 +1,30 @@
+function loadStockMaster() {
+    window.location = "https://github.com/AaronGao2028/stock-master";
+}
+function loadLinkedin() {
+    window.location = "https://www.linkedin.com/in/aaron-gao-485268230/";
+}
+function loadGithub() {
+    window.location = "https://github.com/AaronGao2028";
+}
+function loadEmail() {
+    window.location = "https://mail.google.com/mail/u/0/#inbox";
+}
+function loadHome() {
+    window.location = "https://aarongao2028.github.io/personal-website/#home";
+}
+function loadAbout() {
+    window.location = "https://aarongao2028.github.io/personal-website/#about";
+}
+function loadExperience() {
+    window.location = "https://aarongao2028.github.io/personal-website/#experience";
+}
+function loadProjects() {
+    window.location = "https://aarongao2028.github.io/personal-website/#projects";
+}
+function loadResume() {
+    window.location = "https://drive.google.com/file/d/1KQiL8WFklauch3ljy6u60lDy594KoGm4/view?usp=sharing";
+}
+function loadGameHub() {
+    window.location = "https://game--hub.vercel.app/";
+}
